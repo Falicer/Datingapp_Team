346e3d34d5bd506dd310d2a6f796f52a5c4e3b41
Jesse Dijkman <jesse.dijkman@hva.nl>
2020-06-11 16:31:49 +0200

    Added some basic routes

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express")
 
+// Routes
+const index = require("./routes/index")
+const login = require("./routes/login")
+const register = require("./routes/register")
+
 // Constants
 const PORT = 2000
 
+// Init
 const app = express()
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!")
-})
+// Routes
+app.use("/", index)
+app.use("/login", login)
+app.use("/register", register)
 
 app.listen(PORT, () => console.log(`Server on port: ${PORT}`))
