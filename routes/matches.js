634f6bb2f196f411b5ba0f2c34fe98955f2534eb
Jesse Dijkman <jesse.dijkman@hva.nl>
2020-06-20 18:23:29 +0200

    Can now render the matches on the matches page

--- routes/matches.js
+++ routes/matches.js
@@ -3,8 +3,16 @@ const router = express.Router()
 
 const { checkAuthenticated } = require("../middleware/authentication")
 
+const { getMatches } = require("../utils/matching")
+
 router.get("/", checkAuthenticated, async (req, res) => {
-  res.status(200).send("Matches page")
+  try {
+    const matches = await getMatches(req.user._id)
+
+    res.status(200).render("matches", { matches })
+  } catch (error) {
+    res.status(500).send("Internal Server Error")
+  }
 })
 
 module.exports = router
