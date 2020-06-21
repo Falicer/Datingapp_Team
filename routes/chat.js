930eac6af48e1c583763c86068f66b0351aef7a8
Jesse Dijkman <jesse.dijkman@hva.nl>
2020-06-21 16:20:45 +0200

    Added global "local" variable middleware, updated some routes

--- routes/chat.js
+++ routes/chat.js
@@ -15,17 +15,16 @@ const { addMessage } = require("../utils/chat")
 const { getTrending, searchGiphy } = require("../utils/giphy")
 
 router.get("/:chatId", checkAuthenticated, allowInChat, async (req, res) => {
-  const { _id } = req.user
   const { chatId } = req.params
 
   try {
     const chat = await Chat.findById(chatId)
-    const match = await getMatchFromChatId(chat._id, _id)
+    const match = await getMatchFromChatId(chat._id, req.user)
 
     // It would be really weird if this happened
     if (!match) return res.status(404).send("Page not found")
 
-    res.status(200).render("chat", { userId: _id, chat, otherUser: match.user })
+    res.status(200).render("chat", { chat, otherUser: match.user })
   } catch (error) {
     console.log(error)
     res.status(500).send("Internal Server Error")
@@ -71,8 +70,6 @@ router.post(
   async (req, res) => {
     const { chatId } = req.params
     const { type, content } = req.body
-
-    console.log(type, content)
 
     try {
       await addMessage(chatId, { type, content })
