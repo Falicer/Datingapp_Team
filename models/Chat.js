3d44af527fa82c409e98a1b6d77fce2c1d5dd670
Jesse Dijkman <jesse.dijkman@hva.nl>
2020-06-21 12:50:21 +0200

    Added some fields to message model

--- models/Chat.js
+++ models/Chat.js
@@ -3,19 +3,22 @@ const { model, Schema } = require("mongoose")
 const MessageSchema = new Schema({
   type: String,
   message: String, // Either a link to a gipihy or text message
-})
-
-const ChatSchema = new Schema({
-  matchId: {
-    type: Schema.ObjectId,
-    required: true,
-  },
-  messages: [MessageSchema],
-  createdAt: {
+  date: {
     type: Date,
     default: Date.now,
   },
 })
+
+const ChatSchema = new Schema(
+  {
+    matchId: {
+      type: Schema.ObjectId,
+      required: true,
+    },
+    messages: [MessageSchema],
+  },
+  { timestamps: true }
+)
 
 const Model = model("chat", ChatSchema)
 
